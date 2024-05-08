1639bb4ae358585469280012880f93b763a6f4e1
mesocrisetus <77869459+mesocrisetus@users.noreply.github.com>
2024-05-08 12:48:58 -0500

    Creacion de pagina Consumo

--- api/urls.py
+++ api/urls.py
@@ -13,5 +13,6 @@ from .api import ApiViewSet
 
 ruta = routers.DefaultRouter()
 ruta.register('api/empleados', ApiViewSet, 'api')
+ruta.register('api/consumo', ApiViewSet, 'api')
 
 urlpatterns = ruta.urls
